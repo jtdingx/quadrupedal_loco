834a1db6a8b2f772fdb1ba045145e79e05b844bf
Francesco <f.vezzi@studenti.unipi.it>
2022-07-26 14:23:44 +0200

    use relative path for history file loading

--- unitree_ros_to_real/unitree_legged_real/src/go1_rt_control/src/jumping_controller/read_history.cpp
+++ unitree_ros_to_real/unitree_legged_real/src/go1_rt_control/src/jumping_controller/read_history.cpp
@@ -3,6 +3,24 @@
 #include <complex>
 #include <eigen3/Eigen/Dense>
 
+
+std::string dirname(const std::string& s) {
+    std::string::size_type last = s.find_last_of("/");
+    return std::string(s.begin(), s.begin() + last);
+}
+
+std::string get_file() {
+    char buffer[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX);
+    return std::string(buffer, (count > 0) ? count : 0);
+}
+
+std::string get_history_path(const std::string& s) {
+    std::string file_path = get_file();
+    std::string dir = dirname(file_path);
+    return dir + "/" + s;
+}
+
 void get_data(Eigen::MatrixXd& mat, const std::string& file_name) {
     cnpy::NpyArray history = cnpy::npy_load(file_name);
     double* ptr = history.data<double>();
@@ -15,11 +33,15 @@ int main() {
     using namespace std;
     
     Eigen::MatrixXd loaded_history;
-    get_data(loaded_history, "history.npy");
+    string history_path = get_history_path("history.npy");
+    cout << history_path << endl;
 
-    Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+    // string history_path = get_cwd() + "/" + "history.npy";
+    // get_data(loaded_history, history_path);
 
-    cout << loaded_history.format(CleanFmt) << endl;
+    // Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+
+    // cout << loaded_history.format(CleanFmt) << endl;
 
     return 0;
 }
